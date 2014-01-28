deb4286cd35ee74272410528efa1421d6c113f78
Chandan Singh <singh.chandan4455@gmail.com>
2014-01-28 22:39:33 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style type="text/css">
+* {
+ font-size: 100%;
+ font-family: Arial;
+}
+body{
+    margin:0px;
+    background:white;
+}
+.header-cont {
+    width:100%;
+    position:fixed;
+    top:0px;
+}
+.header-border{
+width:100%;
+border:2px solid #6666FF;
+margin-top:15px;
+}
+.header {
+    height:100px;
+    background:#3399FF;
+    border:1px solid #CCC;
+	width:100%;
+    margin:0px auto;
+	text-align:center;
+}
+.content{
+	width:960px;
+    background: white;
+    border: 1px solid #CCC;
+	top:200px;
+	margin:120px auto;
+}
+.footer{
+	position: fixed;
+    bottom: 0;
+	width:100%;
+	height:50px;
+	background:#3399FF;
+	text-align:center;
+}
+.header-data{
+	margin-top:25px;
+	text-align:center;
+	color:white;
+	font-size:50px;
+}
+.footer-data
+{	
+	margin-bottom:20px;
+	text-align:center;
+	font-size:18px;
+}
+</style>
+</head>
+<body>
+<div class="header-cont">
+	<div class="header">
+		<div class="header-data">
+		CODE JUDGE
+		</div>
+		<div class="header-border">
+		</div>
+	</div>
+</div>
+<div class="footer">
+	<div class="footer-data">
+		<p>Developed by <span style="color:white;">Chandan Singh</span></p>
+	</div>
+</div>
+<body>
+</html>
