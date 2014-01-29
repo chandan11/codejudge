1a64a825b0ce43b90e6f18311f2f57a694b8505f
Chandan Singh <singh.chandan4455@gmail.com>
2014-01-29 18:51:54 +0530

    Update rank.php

--- rank.php
+++ rank.php
@@ -1,3 +1,44 @@
+<?php
+ob_start();
+session_start();
+include "conn_db.php";
+//$name= $_SESSION['sess_username'];
+//echo "yes".$name;
+if((!isset($_SESSION['sess_user_id'])) || (trim($_SESSION['sess_user_id']) == '')) //sess_username or sess_userid not set then redirect to login page.
+{
+	//writing in session for redirecting to the same page after login process.
+	session_regenerate_id();
+	$redirect_url=$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
+	//echo $redirect_url;
+	$_SESSION['redirect_url'] = $redirect_url;
+	session_write_close();
+	header("Location:/codejudge/");
+	exit();
+}
+//checking for wrong url id--> contest code
+if(isset($_GET['id']))
+{
+	$contest_code=$_GET['id'];
+	$result=mysqli_query($con,"SELECT * FROM judge_contest WHERE contest_code='$contest_code'") or die('mysql_error($con)');
+	$count=$result->num_rows;
+	if($count!=1)
+	{
+		header("Location:/codejudge/error.php");
+		exit();	
+	}
+}
+else
+{
+	session_regenerate_id();
+  	$error_msg="You entered a wrong url";
+	$_SESSION['wrong_url'] = $error_msg;
+	session_write_close();
+	flush();
+	header("Location:/codejudge/error.php");
+	exit();	
+}
+?>
+	
 <!DOCTYPE html>
 <html>
 <head>
@@ -175,46 +216,28 @@ color:black;
 </head>
 
 <body>
-<?php
-include "conn_db.php";
-ob_start();
-session_start();
-//$name= $_SESSION['sess_username'];
-//echo "yes".$name;
-if((!isset($_SESSION['sess_user_id'])) || (trim($_SESSION['sess_user_id']) == '')) //sess_username or sess_userid not set then redirect to login page.
-{
-	//writing in session for redirecting to the same page after login process.
-	session_regenerate_id();
-	$redirect_url=$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
-	echo $redirect_url;
-	$_SESSION['redirect_url'] = $redirect_url;
-	session_write_close();
-	header("Location: /codejudge/");
-	exit();
-}
-if(isset($_GET['id']))
-{
-	$contest_code=$_GET['id'];
-	$result=mysqli_query($con,"SELECT * FROM judge_contest WHERE contest_code='$contest_code'") or die('mysql_error($con)');
-	$count=$result->num_rows;
-	//echo $count;
-	if($count!=1)
-	{
-		session_regenerate_id();
-  		$error_msg="You entered a wrong url";
-		$_SESSION['wrong_url'] = $error_msg;
-		session_write_close();
-		header("Location: /codejudge/error.php");
-		exit();	
-	}
-}
-else
-{
-	header("Location: /codejudge/error.php");
-	exit();	
-}
+<!-- begin olark code -->
+<script data-cfasync="false" type='text/javascript'>/*<![CDATA[*/window.olark||(function(c){var f=window,d=document,l=f.location.protocol=="https:"?"https:":"http:",z=c.name,r="load";var nt=function(){
+f[z]=function(){
+(a.s=a.s||[]).push(arguments)};var a=f[z]._={
+},q=c.methods.length;while(q--){(function(n){f[z][n]=function(){
+f[z]("call",n,arguments)}})(c.methods[q])}a.l=c.loader;a.i=nt;a.p={
+0:+new Date};a.P=function(u){
+a.p[u]=new Date-a.p[0]};function s(){
+a.P(r);f[z](r)}f.addEventListener?f.addEventListener(r,s,false):f.attachEvent("on"+r,s);var ld=function(){function p(hd){
+hd="head";return["<",hd,"></",hd,"><",i,' onl' + 'oad="var d=',g,";d.getElementsByTagName('head')[0].",j,"(d.",h,"('script')).",k,"='",l,"//",a.l,"'",'"',"></",i,">"].join("")}var i="body",m=d[i];if(!m){
+return setTimeout(ld,100)}a.P(1);var j="appendChild",h="createElement",k="src",n=d[h]("div"),v=n[j](d[h](z)),b=d[h]("iframe"),g="document",e="domain",o;n.style.display="none";m.insertBefore(n,m.firstChild).id=z;b.frameBorder="0";b.id=z+"-loader";if(/MSIE[ ]+6/.test(navigator.userAgent)){
+b.src="javascript:false"}b.allowTransparency="true";v[j](b);try{
+b.contentWindow[g].open()}catch(w){
+c[e]=d[e];o="javascript:var d="+g+".open();d.domain='"+d.domain+"';";b[k]=o+"void(0);"}try{
+var t=b.contentWindow[g];t.write(p());t.close()}catch(x){
+b[k]=o+'d.write("'+p().replace(/"/g,String.fromCharCode(92)+'"')+'");d.close();'}a.P(2)};ld()};nt()})({
+loader: "static.olark.com/jsclient/loader0.js",name:"olark",methods:["configure","extend","declare","identify"]});
+/* custom configuration goes here (www.olark.com/documentation) */
+olark.identify('9700-375-10-2541');/*]]>*/</script><noscript><a href="https://www.olark.com/site/9700-375-10-2541/contact" title="Contact us" target="_blank">Questions? Feedback?</a> powered by <a href="http://www.olark.com?welcome" title="Olark live chat software">Olark live chat software</a></noscript>
+<!-- end olark code -->
 
-?>
+
 
 <div class="header-cont">
 	<div class="header">
